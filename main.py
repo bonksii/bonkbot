80257bb5a33f9fc4fd68ea4c9bc7ed6fbbbbd95c
bonksii <150732330+bonksii@users.noreply.github.com>
2024-09-26 18:55:46 +0100

    Added websocket logger and changed boost alerts config

--- main.py
+++ main.py
@@ -3,61 +3,50 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from discord.ext import commands, tasks
-from itertools import cycle #defunct
-from discord import app_commands
+import settings
+import subprocess
+import threading
 
-#import logging
-#import logging.handlers
+logger = settings.logging.getLogger("bot")
 
-#defines the intents the bot will use
-intents = discord.Intents.all()
-intents.message_content = True #enables access to message content
+#defining the intents the bot will use
+intents = discord.Intents.all() #default does not allow the functions i needed lol
+intents.message_content = True 
 intents.members = True
 
 
 load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents) #Initialise for hybrid commands  TBD
 
 # event trigger
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.do_not_disturb)
-    print(f'Logged in as {bot.user}')
-    print("The Bot is ready for usage")
-    print("--------------------------")
-    bot.loop.create_task(change_status())
+    #await bot.change_presence(status=discord.Status.do_not_disturb) REDUNDANT
+    logger.info(f"User: {bot.user} (ID: {bot.user.id})")
     try:
         synced_commands = await bot.tree.sync()
-        print(f"Synced {len(synced_commands)} commands.")
+        logger.info(f"Successfully Synced {len(synced_commands)} Commands.")
     except Exception as e:
-        print("An error with syncing application commands has occured: ", e)
+         logger.error("Error syncing App commands has occurred: ", exc_info=e)
+
+def run_logger():
+    subprocess.run(["python", "discordchatlogger.py"])
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
-activities = [
-    discord.Game("bonksii.com"),
-    discord.Game("bonk.io"),
-    discord.Activity(type=discord.ActivityType.listening, name="PinkPantheress"),
-    discord.Activity(type=discord.ActivityType.watching, name="bonksii on Youtube")
-]
-
-# Bot status cycler
-async def change_status():
-    await bot.wait_until_ready()
-    while not bot.is_closed():
-        for activity in activities:
-            await bot.change_presence(status=discord.Status.do_not_disturb, activity=activity)
-            await asyncio.sleep(120)
-
 async def main():
     async with bot:
         await load()
         await bot.start(TOKEN)
+        
+if __name__ == "__main__":
+    logger_thread = threading.Thread(target=run_logger)
+    logger_thread.start()
 
-# Grab token id from the environment field 
+#run
 asyncio.run(main())
