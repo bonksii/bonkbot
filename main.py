ec0b502e1b78fdffc4af29b01fd91e6a0d2ab450
bonksii <150732330+bonksii@users.noreply.github.com>
2024-09-27 01:40:44 +0100

    added ⭐"PIZAZZ"⭐
    This includes the chat logger, background task info which tracks the info of users online statuses between my  2 servers, bot has made  a lot of progress so far
    
    TODO import the hybrid commands for usage 9/27/2024

--- main.py
+++ main.py
@@ -6,47 +6,67 @@ from discord.ext import commands, tasks
 import settings
 import subprocess
 import threading
+import colorama
 
 logger = settings.logging.getLogger("bot")
 
-#defining the intents the bot will use
-intents = discord.Intents.all() #default does not allow the functions i needed lol
-intents.message_content = True 
+
+intents = discord.Intents.all()  # default does not allow the functions i need lol
+intents.message_content = True
 intents.members = True
 
 
 load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix='!', intents=intents) #Initialise for hybrid commands  TBD
+bot = commands.Bot(
+    command_prefix="!", intents=intents
+)  # TODO Initialise for hybrid commands later TBD
 
-# event trigger
+
 @bot.event
 async def on_ready():
-    #await bot.change_presence(status=discord.Status.do_not_disturb) REDUNDANT
+    # This isnt needed but it looks cool so im keeping it :)
+    print(
+        f"""{colorama.Fore.BLUE}
+
+██████╗  ██████╗ ███╗   ██╗██╗  ██╗██████╗  ██████╗ ████████╗
+██╔══██╗██╔═══██╗████╗  ██║██║ ██╔╝██╔══██╗██╔═══██╗╚══██╔══╝
+██████╔╝██║   ██║██╔██╗ ██║█████╔╝ ██████╔╝██║   ██║   ██║   
+██╔══██╗██║   ██║██║╚██╗██║██╔═██╗ ██╔══██╗██║   ██║   ██║   
+██████╔╝╚██████╔╝██║ ╚████║██║  ██╗██████╔╝╚██████╔╝   ██║   
+╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝  
+                                                                      
+\n\n {colorama.Fore.RESET}\n"""  # Actuall beginning for bot synchronisation and initialisation
+    )
     logger.info(f"User: {bot.user} (ID: {bot.user.id})")
     try:
         synced_commands = await bot.tree.sync()
         logger.info(f"Successfully Synced {len(synced_commands)} Commands.")
     except Exception as e:
-         logger.error("Error syncing App commands has occurred: ", exc_info=e)
+        logger.error("Error syncing App commands has occurred: ", exc_info=e)
+
 
 def run_logger():
     subprocess.run(["python", "discordchatlogger.py"])
+
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
+
+# Magic. Do not touch.
 async def main():
     async with bot:
         await load()
         await bot.start(TOKEN)
-        
+
+
 if __name__ == "__main__":
     logger_thread = threading.Thread(target=run_logger)
     logger_thread.start()
 
-#run
+
 asyncio.run(main())
