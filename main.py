25d57f5c760d0acfbaa1ce787c3aece542c3863f
bonksii <150732330+bonksii@users.noreply.github.com>
2024-10-23 04:04:49 +0100

    Prefix commands duplication error

--- main.py
+++ main.py
@@ -2,42 +2,36 @@ import discord
 import os
 from dotenv import load_dotenv
 import asyncio
-from discord.ext import commands, tasks
-import settings
+from discord.ext import commands
 import subprocess
 import threading
 import colorama
+import signal
+from discordchatlogger import connect_to_discord
+import cogs.settings as settings
 
 logger = settings.logging.getLogger("bot")
 
-
-intents = discord.Intents.all()  # default does not allow the functions i need lol
+intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
-
 
 load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
 
-bot = commands.Bot(
-    command_prefix="!", intents=intents
-)  # TODO Initialise for hybrid commands later TBD
-
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    # This isnt needed but it looks cool so im keeping it :)
-    print(
-        f"""{colorama.Fore.BLUE}
+    print(f"""{colorama.Fore.BLUE}
+    ██████╗  ██████╗ ███╗   ██╗██╗  ██╗██████╗  ██████╗ ████████╗
+    ██╔══██╗██╔═══██╗████╗  ██║██║ ██╔╝██╔══██╗██╔═══██╗╚══██╔══╝
+    ██████╔╝██║   ██║██╔██╗ ██║█████╔╝ ██████╔╝██║   ██║   ██║   
+    ██╔══██╗██║   ██║██║╚██╗██║██╔═██╗ ██╔══██╗██║   ██║   ██║   
+    ██████╔╝╚██████╔╝██║ ╚████║██║  ██╗██████╔╝╚██████╔╝   ██║   
+    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝  
+          {colorama.Fore.RESET}\n""")
 
-██████╗  ██████╗ ███╗   ██╗██╗  ██╗██████╗  ██████╗ ████████╗
-██╔══██╗██╔═══██╗████╗  ██║██║ ██╔╝██╔══██╗██╔═══██╗╚══██╔══╝
-██████╔╝██║   ██║██╔██╗ ██║█████╔╝ ██████╔╝██║   ██║   ██║   
-██╔══██╗██║   ██║██║╚██╗██║██╔═██╗ ██╔══██╗██║   ██║   ██║   
-██████╔╝╚██████╔╝██║ ╚████║██║  ██╗██████╔╝╚██████╔╝   ██║   
-╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝  
-{colorama.Fore.RESET}\n"""  # Actuall beginning for bot synchronisation and initialisation
-    )
     logger.info(f"User: {bot.user} (ID: {bot.user.id})")
     try:
         synced_commands = await bot.tree.sync()
@@ -45,27 +39,22 @@ async def on_ready():
     except Exception as e:
         logger.error("Error syncing App commands has occurred: ", exc_info=e)
 
-
-def run_logger():
-    subprocess.run(["python", "discordchatlogger.py"])
-
-
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
+    await bot.load_extension('jishaku')
 
+def start_logger():  
+    connect_to_discord()
 
 # Magic. Do not touch.
 async def main():
     async with bot:
         await load()
+        logger_thread = threading.Thread(target=connect_to_discord, daemon=True)
+        logger_thread.start()
         await bot.start(TOKEN)
 
-
 if __name__ == "__main__":
-    logger_thread = threading.Thread(target=run_logger)
-    logger_thread.start()
-
-
-asyncio.run(main())
+    asyncio.run(main())
